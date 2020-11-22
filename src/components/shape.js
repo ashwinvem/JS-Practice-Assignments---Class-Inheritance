6bea451492fafb381af5867ba3440843960e7480
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-11-22 12:03:40 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
-// declare class
 
+// declare class
+class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("draw Shape");
+  }
+  calculateArea() {
+    console.log("calculate Area of Shape");
+  }
+}
 // export class using module.exports
+module.exports = Shape;
